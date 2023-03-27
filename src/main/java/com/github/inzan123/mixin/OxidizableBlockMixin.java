ea2b516ea4fdc4b9b2ea1e15069c6854eb999c78
Zan <69921455+InZan123@users.noreply.github.com>
2023-03-27 20:24:32 +0200

    poor copper block implementation

---
+++ src/main/java/com/github/inzan123/mixin/OxidizableBlockMixin.java
@@ -0,0 +1,74 @@
+package com.github.inzan123.mixin;
+
+import com.github.inzan123.SimulateRandomTicks;
+import com.github.inzan123.UnloadedActivity;
+import net.minecraft.block.*;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static java.lang.Math.pow;
+
+@Mixin(OxidizableBlock.class)
+public class OxidizableBlockMixin implements SimulateRandomTicks, Oxidizable {
+
+    @Override
+    public double getOdds(ServerWorld world, BlockPos pos) {
+        return 0.05688889f;
+    }
+    @Shadow
+    public Oxidizable.OxidationLevel getDegradationLevel() {
+        return null;
+    }
+
+    @Override public boolean canSimulate(BlockState state, ServerWorld world, BlockPos pos) {
+        return true;
+    }
+
+    @Override
+    public void simulateTime(BlockState state, ServerWorld world, BlockPos pos, Random random, long timePassed, int randomTickSpeed) {
+
+        double randomPickChance = 1.0-pow(1.0 - 1.0 / 4096.0, randomTickSpeed);
+
+        double tryDegradeOdds = getOdds(world, pos);
+
+        BlockPos blockPos;
+        int degradationLevel = ((Enum)this.getDegradationLevel()).ordinal();
+        float nearbyBlocks = 0;
+        Iterator<BlockPos> iterator = BlockPos.iterateOutwards(pos, 4, 4, 4).iterator();
+        while (iterator.hasNext() && (blockPos = iterator.next()).getManhattanDistance(pos) <= 4) {
+            if (blockPos.equals(pos) || !((world.getBlockState(blockPos).getBlock()) instanceof Degradable)) continue;
+            nearbyBlocks++;
+        }
+        float degradeOdds = 1 / (nearbyBlocks + 1);
+        float degradeOdds2 = degradeOdds * degradeOdds * 0.75f;
+
+        double totalOdds = randomPickChance * tryDegradeOdds * degradeOdds2;
+
+        int currentAge = (this.getDegradationLevel()).ordinal();
+
+        int ageDifference = 3-currentAge;
+
+        int ageAmount = getOccurrences(timePassed, totalOdds, ageDifference, random);
+
+        if (ageAmount == 0) return;
+
+        degrade(ageAmount, state, world, pos);
+    }
+
+    public void degrade(int steps, BlockState state, ServerWorld world, BlockPos pos) {
+        this.getDegradationResult(state).ifPresent(state2 -> {
+            world.setBlockState(pos, state2);
+            int newSteps = steps - 1;
+            if (newSteps != 0) {
+                degrade(newSteps, state2, world, pos);
+                // I am currently having a breakdown. Who decided copper blocks should be this complicated??? and Why??? WHY COULDNT IT JUST BE LIKE NORMAL BLOCKS????
+            }
+        });
+    }
+}
