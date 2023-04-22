f16425d889d24e658c2f228cad3473156bbb7d00
Zan <69921455+InZan123@users.noreply.github.com>
2023-04-23 01:12:06 +0200

    minuscule improvements

--- src/main/java/com/github/inzan123/mixin/EntityMixin.java
+++ src/main/java/com/github/inzan123/mixin/EntityMixin.java
@@ -1,9 +1,14 @@
 package com.github.inzan123.mixin;
 
 import com.github.inzan123.*;
+import dev.onyxstudios.cca.api.v3.component.ComponentAccess;
 import net.minecraft.entity.Entity;
+import net.minecraft.server.command.CommandOutput;
+import net.minecraft.util.Nameable;
 import net.minecraft.world.World;
+import net.minecraft.world.entity.EntityLike;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,14 +17,15 @@ import static com.github.inzan123.MyComponents.LASTENTITYTICK;
 import static java.lang.Long.max;
 
 @Mixin(Entity.class)
-public abstract class EntityMixin implements SimulateEntity {
+public abstract class EntityMixin implements Nameable, EntityLike, CommandOutput, SimulateEntity, ComponentAccess {
+
+    @Shadow public World world;
     @Inject(at = @At("HEAD"), method = "tick")
     public void tickMovement(CallbackInfo ci) {
-        Entity entity = (Entity)(Object)this;
 
-        LongComponent lastTick = entity.getComponent(LASTENTITYTICK);
+        LongComponent lastTick = this.getComponent(LASTENTITYTICK);
 
-        World world = entity.world;
+        World world = this.world;
 
         long currentTime = world.getTimeOfDay();
 
@@ -30,7 +36,7 @@ public abstract class EntityMixin implements SimulateEntity {
             int differenceThreshold = UnloadedActivity.instance.config.tickDifferenceThreshold;
 
             if (timeDifference > differenceThreshold)
-                TimeMachine.simulateEntity(entity, timeDifference);
+                TimeMachine.simulateEntity((Entity)(Object)this, timeDifference);
         }
         lastTick.setValue(currentTime);
     }
