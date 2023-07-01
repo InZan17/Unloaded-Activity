20e91c5e604fa3afa0818220dced08539cc461ac
Zan <69921455+InZan123@users.noreply.github.com>
2023-07-01 18:07:15 +0200

    Fixed fast growing torch flowers

---
+++ src/main/java/com/github/inzan123/mixin/blocks/TorchFlowerMixin.java
@@ -0,0 +1,20 @@
+package com.github.inzan123.mixin.blocks;
+
+import net.minecraft.block.CropBlock;
+import net.minecraft.block.TorchflowerBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TorchflowerBlock.class)
+public class TorchFlowerMixin extends CropBlock {
+    public TorchFlowerMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public double getOdds(ServerWorld world, BlockPos pos) {
+        float f = getAvailableMoisture(this, world, pos);
+        return (1.0/(double)((int)(25.0F / f) + 1))/3;
+    }
+}
