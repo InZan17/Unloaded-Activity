4d091ac0a9cd366a08c4eb7e383b56b89866cf74
Zan <69921455+InZan17@users.noreply.github.com>
2024-06-17 23:54:54 +0200

    fix commands on neoforge

--- neoforge/src/main/java/com/github/inzan17/neoforge/UnloadedActivityNeoForge.java
+++ neoforge/src/main/java/com/github/inzan17/neoforge/UnloadedActivityNeoForge.java
@@ -9,10 +9,14 @@ import com.github.inzan17.UnloadedActivity;
 import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.neoforged.neoforge.client.ConfigScreenHandler;
 
+import static net.neoforged.neoforge.common.NeoForge.EVENT_BUS;
+
 @Mod(UnloadedActivity.MOD_ID)
 public final class UnloadedActivityNeoForge {
     public UnloadedActivityNeoForge() {
         UnloadedActivity.init();
+
+        EVENT_BUS.register(new NeoForgeEventHandler());
 
         if (ModList.get().isLoaded("cloth_config")) {
             ModLoadingContext.get().registerExtensionPoint(
