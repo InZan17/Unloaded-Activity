0446e5d4aa8e71af46a4350a35d638488c67db00
Zan <69921455+InZan17@users.noreply.github.com>
2024-07-22 01:45:28 +0200

    fix neoforge crash when simulating beetroots

--- common/src/main/java/lol/zanspace/unloadedactivity/mixin/chunk/randomTicks/BeetrootsMixin.java
+++ common/src/main/java/lol/zanspace/unloadedactivity/mixin/chunk/randomTicks/BeetrootsMixin.java
@@ -1,5 +1,6 @@
 package lol.zanspace.unloadedactivity.mixin.chunk.randomTicks;
 
+import lol.zanspace.unloadedactivity.ExpectPlatform;
 import net.minecraft.block.BeetrootsBlock;
 import net.minecraft.block.CropBlock;
 import net.minecraft.server.world.ServerWorld;
@@ -14,7 +15,11 @@ public abstract class BeetrootsMixin extends CropBlock{
 
     @Override
     public double getOdds(ServerWorld world, BlockPos pos) {
-        float f = getAvailableMoisture(this, world, pos);
+        #if MC_VER >= MC_1_21
+        float f = ExpectPlatform.getAvailableMoisture(world.getBlockState(pos), world, pos);
+        #else
+        float f = CropBlockInvoker.getAvailableMoisture(this, world, pos);
+        #endif
         return (1.0/(double)((int)(25.0F / f) + 1))/3;
     }
 }
