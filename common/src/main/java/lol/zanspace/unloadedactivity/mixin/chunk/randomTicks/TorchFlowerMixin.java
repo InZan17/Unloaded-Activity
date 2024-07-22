6269a955b8ceed04d4641b31dd21071acfe356b3
Zan <69921455+InZan17@users.noreply.github.com>
2024-07-22 02:16:45 +0200

    fix torch flowers too

--- common/src/main/java/lol/zanspace/unloadedactivity/mixin/chunk/randomTicks/TorchFlowerMixin.java
+++ common/src/main/java/lol/zanspace/unloadedactivity/mixin/chunk/randomTicks/TorchFlowerMixin.java
@@ -1,5 +1,6 @@
 package lol.zanspace.unloadedactivity.mixin.chunk.randomTicks;
 
+import lol.zanspace.unloadedactivity.ExpectPlatform;
 import org.spongepowered.asm.mixin.Mixin;
 
 #if MC_VER >= MC_1_19_4
@@ -16,7 +17,11 @@ public class TorchFlowerMixin extends CropBlock {
 
     @Override
     public double getOdds(ServerWorld world, BlockPos pos) {
-        float f = getAvailableMoisture(this, world, pos);
+        #if MC_VER >= MC_1_21
+        float f = ExpectPlatform.getAvailableMoisture(world.getBlockState(pos), world, pos);
+        #else
+        float f = CropBlockInvoker.getAvailableMoisture(this, world, pos);
+        #endif
         return (1.0/(double)((int)(25.0F / f) + 1))/3;
     }
 }
